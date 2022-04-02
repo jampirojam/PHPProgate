d2c840d1925412bf3a8c46f0758913cb48eb1223
M. Machrush Aliy Sirojjam Mushlich <jampirojam@gmail.com>
2022-04-02 10:38:29 +0700

    init

--- PHP Dojo I/11. The Most Expensive Items/index.php
+++ PHP Dojo I/11. The Most Expensive Items/index.php
@@ -7,9 +7,16 @@ $menus = array(
 
 // Write your code below
 $total = 0;
+$terbesar = 0;
+$namamenu = '';
 foreach ($menus as $menu){
   echo $menu['name'].' is $'.$menu['price'].'<br>';
   $total += $menu['price'];
+  if ($terbesar < $menu['price']){
+    $terbesar = $menu['price'];
+    $namamenu = $menu['name'];
+  }
 }
-echo 'The total price is $'.$total;
+echo 'The total price is $'.$total.'<br>';
+echo 'The most expensive item is '.$namamenu.' with the price $'.$terbesar;
 ?>
